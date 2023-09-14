d36e945cc9e141fbd875968fc829850521f6e205
Char <charlesvader2@gmail.com>
2023-09-14 16:57:16 +0200

    new script

---
+++ Assets/example.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hekk");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
